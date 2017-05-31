062c5d3802f08cc88eb8d8001601ac491d18fc1f
SungYong Jang <jsy@adain.kr>
2017-06-01 04:34:30 +0900

    update node 4.8.3

--- tests/build_it.sh
+++ tests/build_it.sh
@@ -3,11 +3,11 @@ set -x
 
 : ${NODE_VERSION?"NODE_VERSION has not been set."}
 
-docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:node-${NODE_VERSION}-base" ../base && \
-  docker tag "abernix/meteord:node-${NODE_VERSION}-base" abernix/meteord:base
-docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:node-${NODE_VERSION}-onbuild" ../onbuild && \
-  docker tag "abernix/meteord:node-${NODE_VERSION}-onbuild" abernix/meteord:onbuild
-docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:node-${NODE_VERSION}-devbuild" ../devbuild && \
-  docker tag "abernix/meteord:node-${NODE_VERSION}-devbuild" abernix/meteord:devbuild
-docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:node-${NODE_VERSION}-binbuild" ../binbuild && \
-  docker tag "abernix/meteord:node-${NODE_VERSION}-binbuild" abernix/meteord:binbuild
+docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "adain/meteord:node-${NODE_VERSION}-base" ../base && \
+  docker tag "adain/meteord:node-${NODE_VERSION}-base" adain/meteord:base
+docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "adain/meteord:node-${NODE_VERSION}-onbuild" ../onbuild && \
+  docker tag "adain/meteord:node-${NODE_VERSION}-onbuild" adain/meteord:onbuild
+docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "adain/meteord:node-${NODE_VERSION}-devbuild" ../devbuild && \
+  docker tag "adain/meteord:node-${NODE_VERSION}-devbuild" adain/meteord:devbuild
+docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "adain/meteord:node-${NODE_VERSION}-binbuild" ../binbuild && \
+  docker tag "adain/meteord:node-${NODE_VERSION}-binbuild" adain/meteord:binbuild
