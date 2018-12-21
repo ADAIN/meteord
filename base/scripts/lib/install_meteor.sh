e3350aa3f6acb8011071dbb8b4115d29c8a2b60c
SungYong Jang <jsy@adain.kr>
2018-12-21 10:34:47 +0900

    update install_meteor.sh

--- base/scripts/lib/install_meteor.sh
+++ base/scripts/lib/install_meteor.sh
@@ -1,3 +1,11 @@
 set -e
 
+# meteor installer doesn't work with the default tar binary
+RUN apt-get install -y bsdtar \
+    && cp $(which tar) $(which tar)~ \
+    && ln -sf $(which bsdtar) $(which tar)
+
 curl -sL https://install.meteor.com | sed s/--progress-bar/-sL/g | /bin/sh
+
+# put back the original tar
+RUN mv $(which tar)~ $(which tar)
